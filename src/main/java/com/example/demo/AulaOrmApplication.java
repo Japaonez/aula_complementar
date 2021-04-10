9a7f36da5038a2d4f9947bbe95aa3d37d2a92682
Jhonata <japa_jkr@hotmail.com>
2021-04-10 14:19:38 -0300

    Gravação com POST

--- src/main/java/com/example/demo/AulaOrmApplication.java
+++ src/main/java/com/example/demo/AulaOrmApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.model.Aluno;
 import com.example.demo.model.Disciplina;
+import com.example.demo.repository.AlunoRepository;
 import com.example.demo.repository.DisciplinaRepository;
 
 @SpringBootApplication
@@ -19,6 +21,8 @@ public class AulaOrmApplication implements CommandLineRunner{
 	
 	@Autowired
 	private DisciplinaRepository disciplinaRepository;
+	@Autowired
+	private AlunoRepository alunoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -26,7 +30,14 @@ public class AulaOrmApplication implements CommandLineRunner{
 		Disciplina d2 = new Disciplina("Estrutura de Dados", 64);
 		Disciplina d3 = new Disciplina("Engenharia de Software", 64);
 		
+		Aluno a1 = new Aluno(0, "João", "joao@gmail.com", 123, "CC");
+		Aluno a2 = new Aluno(0, "André", "andre@gmail.com", 234, "CC");
+		Aluno a3 = new Aluno(0, "Formigão", "formiga@gmail.com", 154, "CC");
+		Aluno a4 = new Aluno(0, "Maria", "maria@gmail.com", 641, "CC");
+		Aluno a5 = new Aluno(0, "Marcos", "marcos@gmail.com", 100, "CC");
+		
 		disciplinaRepository.saveAll(Arrays.asList(d1, d2, d3));
+		alunoRepository.saveAll(Arrays.asList(a1, a2, a3, a4, a5));
 		
 	}
 
